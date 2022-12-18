a11e706f9f31a4804a5b732e50f97d39971fa087
bhaveshcodes <120417108+bhaveshcodes@users.noreply.github.com>
2022-12-19 03:06:38 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <div className="container">
-            <h1 className="title">Testing Title</h1>
+            <h1 className="title">Free Courses Project</h1>
         </div>
     )
 }
