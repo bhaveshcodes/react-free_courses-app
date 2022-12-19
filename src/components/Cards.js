ed519ef0d7d0fb606fc60acdad193542828325c9
bhaveshcodes <120417108+bhaveshcodes@users.noreply.github.com>
2022-12-19 22:05:31 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios"
 import EachCard from './EachCard';
-import Loader from "./Loader"
+//import Loader from "./Loader"
 
 const Cards = (props) => {
 
     console.log(props.searchQuery + "<<<<yayy");
     const [data, setData] = useState([])
     // const [query, setQuery] = useState('')
-    const [isLoading, setIsLoading] = useState(true)
+ //   const [isLoading, setIsLoading] = useState(true)
 
 
     useEffect(() => {
         fetchData();
         async function fetchData() {
-            setIsLoading(true)
+            //setIsLoading(true)
             // const fetchData = async () => {
             // const result1 = await axios(`https://courses-project-api.vercel.app/api?c=${props.searchQuery}`)
             const result = await axios(`https://course-mongo-db-api.vercel.app/api?s=${props.searchQuery}`)
@@ -22,7 +22,7 @@ const Cards = (props) => {
             // console.log(query);
             console.log(result.data);
             setData(result.data)
-            setIsLoading(false)
+            //setIsLoading(false)
         }
     }, [props.searchQuery])
 
@@ -30,9 +30,9 @@ const Cards = (props) => {
     return (
 
         // const loading=var;
-        isLoading ?(<Loader />) :(data.map(card => <EachCard key={card._id} title={card.name} description={card.description} thumbnail={card.thumbnail} ytLink={card.youtube_link} author={card.author} views={card.views}  btn_txt={"Learn Now"} />))
+        //isLoading ?(<Loader />) :(data.map(card => <EachCard key={card._id} title={card.name} description={card.description} thumbnail={card.thumbnail} ytLink={card.youtube_link} author={card.author} views={card.views}  btn_txt={"Learn Now"} />))
         // <h1>Loading...</h1>
-        
+        data.map(card => <EachCard key={card._id} title={card.name} description={card.description} thumbnail={card.thumbnail} ytLink={card.youtube_link} author={card.author} views={card.views}  btn_txt={"Learn Now"} />)
     )
 }
 export default Cards
